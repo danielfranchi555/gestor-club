8cf8b498edcea63789a4cda3edcb3eee8ef590bd
danielfranchi555 <danielfranchi3@gmail.com>
2024-08-21 20:52:37 -0300

    .env

--- src/app/callback/route.js
+++ src/app/callback/route.js
@@ -14,5 +14,5 @@ export async function GET(request) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect('https://gestor-club.vercel.app/');
+  return NextResponse.redirect(requestUrl.origin);
 }
